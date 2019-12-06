e5f64f8fab96371cf605d6f2b4d97f85a749d37e
Andreas Behrend <andreas.behrend@check24.de>
2019-12-06 17:04:05 +0100

    Initial Commit

---
+++ Remember to Drink/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Remember to Drink
+//
+//  Created by Andreas Behrend on 06.12.19.
+//  Copyright © 2019 Andreas Behrend. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
